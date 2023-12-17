b48ad521246867d1cf0833e29b2f3e40917be44b
wilker22 <wilker.silva@codevasf.gov.br>
2023-12-17 16:54:14 -0300

    template admin implementado

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Controllers\Super\HomeController;
 use CodeIgniter\Router\RouteCollection;
 
 /**
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+$routes->get('super', [HomeController::class, 'index'], ['as' => 'super.home']);
