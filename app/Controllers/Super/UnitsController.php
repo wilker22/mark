7fdaa5646703ae882f582af77ceecba6e014f57f
wilker22 <wilker.silva@codevasf.gov.br>
2023-12-19 21:56:52 -0300

    20.11 implementado Html Table Class

--- app/Controllers/Super/UnitsController.php
+++ app/Controllers/Super/UnitsController.php
@@ -4,17 +4,52 @@ namespace App\Controllers\Super;
 
 use App\Controllers\BaseController;
 use App\Entities\Unit;
+use App\Libraries\UnitService;
 use App\Models\UnitModel;
+use CodeIgniter\Config\Factories;
 
 class UnitsController extends BaseController
 {
+
+    private UnitService $unitService;
+    /** @var UnitModel */
+    private UnitModel $unitModel;
+
+    public function __construct()
+    {
+        $this->unitService = Factories::class(UnitService::class);
+        $this->unitModel = model(UnitModel::class);
+    }
+    /**
+     * Renderiza a view para gerenciar as uniadades
+     *
+     * @return RendererInterface
+     */
     public function index()
     {
-        $data =   [
+        $data = [
             'title' => 'Unidades',
-            'units' => model(UnitModel::class)->findAll(),
+            'units' => $this->unitService->renderUnits()
         ];
 
-        return view ('Back/Units/index', $data);
+        return view('Back/Units/index', $data);
+    }
+
+    /**
+     * Renderiza a view para editar o registro
+     *
+     * @param integer $id
+     * @return RendererInterface
+     */
+    public function edit(int $id)
+    {
+        
+        $data = [
+            'title'         => 'Editar unidade',
+            'unit'          => $unit = $this->unitModel->findOrFail($id),
+            //'timesInterval' => $this->unitService->renderTimesInterval($unit->servicetime)
+        ];
+
+        return view('Back/Units/edit', $data);
     }
 }
--- app/Views/Back/Units/index.php
+++ app/Views/Back/Units/index.php
@@ -22,47 +22,15 @@
 <!-- Begin Page Content -->
 <div class="container-fluid">
 
-    <!-- Page Heading -->
-    <h1 class="h3 mb-4 text-gray-800"><?php echo $title ?? 'Home'; ?></h1>
-
-    <!-- Page Heading -->
-    <h1 class="h3 mb-2 text-gray-800">Tables</h1>
-    <p class="mb-4">DataTables is a third party plugin that is used to generate the demo table below.
-        For more information about DataTables, please visit the <a target="_blank" href="https://datatables.net">official DataTables documentation</a>.</p>
-
-    <!-- DataTales Example -->
+        <!-- DataTales Example -->
     <div class="card shadow mb-4">
         <div class="card-header py-3">
             <h6 class="m-0 font-weight-bold text-primary"><?php echo $title ?></h6>
+            <a href="<?php echo route_to('units.new')?>" class="btn btn-success btn-sm float-right">Nova</a>
         </div>
         <div class="card-body">
             <div class="table-responsive">
-                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                    <thead>
-                        <tr>
-                            <th>Nome</th>
-                            <th>E-mail</th>
-                            <th>Telefone</th>
-                            <th>Início</th>
-                            <th>Fim</th>
-                            <th>Criado</th>
-                        </tr>
-                    </thead>
-
-                    <tbody>
-                        <?php foreach ($units as $unit) : ?>
-                            <tr>
-                                <td><?php echo $unit->name ?></td>
-                                <td><?php echo $unit->email ?></td>
-                                <td><?php echo $unit->phone ?></td>
-                                <td><?php echo $unit->starttime ?></td>
-                                <td><?php echo $unit->eendtime ?></td>
-                                <td><?php echo $unit->created_at ?></td>
-                            </tr>
-                        <?php endforeach; ?>
-
-                    </tbody>
-                </table>
+                   <?php echo $units ?>
             </div>
         </div>
     </div>
